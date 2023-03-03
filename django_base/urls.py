1e7d20c178b8bd961016c7e3906d4a9143d44fb1
Rodrik998 <rodrigo_trillo@hotmail.com>
2023-03-02 22:28:09 -0300

    .

--- django_base/urls.py
+++ django_base/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.conf.urls.static import static
 from django.conf import settings
@@ -24,4 +24,7 @@ from django_base.views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+
+    path('include', )
+
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
