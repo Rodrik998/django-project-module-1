5776fc41c4648df3b9c7dd9e2cc9daa53480acee
Rodrik998 <rodrigo_trillo@hotmail.com>
2023-03-16 20:36:04 -0300

    .

--- django_base/urls.py
+++ django_base/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
 
+    path('news/', include('news.urls')),
     path('users/', include('users.urls')),
 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
